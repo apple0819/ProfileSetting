5ca762cb37760e567f7c85ac1fcbd8e9406fd7f9
apple0819 <apple0819@users.noreply.github.com>
2020-03-20 10:15:26 +0900

    BaseActivity 추가 + 메인액티비티 연동

---
+++ app/src/main/java/com/example/profilesetting/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.example.profilesetting;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+
+    public abstract void setValues();
+
+
+}
---
+++ app/src/main/java/com/example/profilesetting/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.profilesetting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupEvents();
+        setValues();
+
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
